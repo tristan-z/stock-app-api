7234e8b0e3adbdce482b79b5acb5f6651a82ad62
clark <clakeyman@gmail.com>
2021-10-27 22:32:03 -0700

    add dummy data to root response

--- routes/index.py
+++ routes/index.py
@@ -1,10 +1,17 @@
 from . import routes
 from flask import jsonify
+import numpy
 
+data = numpy.repeat({
+  "symbol": "AAPL 1",
+  "name": "Apple, Inc.",
+  "price": 149.26,
+  "change": 0.34,
+}, 30)
 
 @routes.route("/")
 def index():
-    response = jsonify("Api Active")
+    response = jsonify(message=data.tolist())
 
     # Enable Access-Control-Allow-Origin
     response.headers.add("Access-Control-Allow-Origin", "*")
